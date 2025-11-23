52cabb3c299e83e6f25b8aa49eff953058782d3b
Anup Mohan <thisisanup@gmail.com>
2025-11-23 00:07:26 +0000

    Fix missing imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import Layout from './components/Layout';
+import Hero from './components/Hero';
+import Gallery from './components/Gallery';
+import About from './components/About';
+import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Blog from './components/Blog';
 function App() {
